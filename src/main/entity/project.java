c7651ce1faca8b09e92acdfc73f6eb80f718a6b9
Xerma <151359440+Xerma1@users.noreply.github.com>
2025-04-16 12:18:49 +0800

    Create project.java

---
+++ src/main/entity/project.java
@@ -0,0 +1,20 @@
+package main.entity;
+
+import java.util.Map;
+
+public class project {
+    private String projectName;
+    private String neighbourhood;
+    private Map<String, Integer> flatTypes; // Map of flat types and their respective counts
+    private String openDate;
+    private String closeDate;
+    private String manager;
+    //private String officer;
+    //private EnquiryList enquiries;
+    private boolean visibility; // True if project is open for application, false otherwise 
+
+    //constructor
+    public project() {
+    }
+
+}

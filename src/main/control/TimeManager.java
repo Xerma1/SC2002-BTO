ddfb5264fe56f7a27fc12df051a6fef21987b39a
Xerma <151359440+Xerma1@users.noreply.github.com>
2025-04-16 12:19:43 +0800

    Update TimeManager.java

--- src/main/control/TimeManager.java
+++ src/main/control/TimeManager.java
@@ -1,28 +1,26 @@
 package main.control;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter; // Handles date and time formatting
+import java.time.LocalDate; // LocalDate class to work with dates
+import java.time.LocalDateTime; // LocalDateTime class to work with dates and time
+import java.time.format.DateTimeFormatter; // Formats date and time
 import java.time.format.DateTimeParseException; // Handles exceptions that may occur during date parsing
 
-// Uses local timezone for simplicity
-public class TimeManager {
+public class timeManager {
     public static String timeNow() {
         LocalDateTime date = LocalDateTime.now(); // Get current date and time
         DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"); // Defining format of date and time
         String formattedDate = date.format(format); // Applying format to date
         return formattedDate;
     }
-
     public static boolean isValidDate(String openDate, String closeDate) {
         // Checks if the current date now is within openDate and closeDate
         LocalDate date = LocalDate.now(); 
         DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy"); // Defining format of date and time
         try {
-            LocalDate open = LocalDate.parse(openDate, format); // Changing openDate from String to LocalDate
-            LocalDate close = LocalDate.parse(closeDate, format); // Changing endDate from String to LocalDate
+            LocalDate open = LocalDate.parse(openDate, format); // Parsing openDate
+            LocalDate close = LocalDate.parse(closeDate, format); // Parsing closeDate
 
-            return !(date.isBefore(open) || date.isAfter(close)); // Returns true if current date is within range inclusive
+            return !(date.isBefore(open) || date.isAfter(close)); // Returns true if within date range inclusive
 
         } catch (DateTimeParseException e) { // Handles exceptions that may occur during date parsing
             System.out.println("Invalid date format. Please use 'dd/MM/yyyy'.");
